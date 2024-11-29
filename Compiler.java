8a4a83adc3ba4e8f0948c993324b784fe008a3ba
Matheus Freire <Matheusfhf678@gmail.com>
2024-11-29 00:12:08 -0300

    Adicionando saida do compilador

--- Compiler.java
+++ Compiler.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 
 public class Compiler {
+  static boolean erro = false;
   public static void main(String[] args) {
     if (args.length == 0) {
       System.out.println("Por favor, forneça o caminho do arquivo como argumento.");
@@ -14,6 +15,9 @@ public class Compiler {
       do {
         t = lexer.scan();
         System.out.println(t.toString());
+        if (t.TAG == Tag.ERROR){
+          erro = true;
+        }
       } while (t.TAG != Tag.EOF);
 
       System.out.println();
@@ -23,5 +27,15 @@ public class Compiler {
       // TODO Auto-generated catch block
       e.printStackTrace();
     }
+    System.out.println();
+    System.out.println("---------- Compilation Result -----------");
+    if(erro == true){
+      System.out.println(" Compilation ERROR ");
+      System.out.println("-----------------------------------------");
+    }else{
+      System.out.println(" Compilation SUCCESS! ");
+      System.out.println("-----------------------------------------");
+    }
+
   }
 }
