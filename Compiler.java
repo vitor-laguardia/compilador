85718426c5d3edc027302ce45d35952130b60f60
Vitor <vitor.laguardia@hotmail.com>
2025-02-04 16:06:41 -0300

    alterado a main para instanciar o parser

--- Compiler.java
+++ Compiler.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 
 public class Compiler {
   static boolean erro = false;
+
   public static void main(String[] args) {
     if (args.length == 0) {
       System.out.println("Por favor, forneça o caminho do arquivo como argumento.");
@@ -10,32 +11,13 @@ public class Compiler {
 
     try {
       Lexer lexer = new Lexer(args[0]);
-      Token t = null;
 
-      do {
-        t = lexer.scan();
-        System.out.println(t.toString());
-        if (t.TAG == Tag.ERROR){
-          erro = true;
-        }
-      } while (t.TAG != Tag.EOF);
+      Parser parser = new Parser(lexer);
 
-      System.out.println();
-      System.out.println("---------- Symbol Table -----------");
-      SymbolTable.printTable();
+      parser.begin();
     } catch (IOException e) {
       // TODO Auto-generated catch block
       e.printStackTrace();
     }
-    System.out.println();
-    System.out.println("---------- Compilation Result -----------");
-    if(erro == true){
-      System.out.println(" Compilation ERROR ");
-      System.out.println("-----------------------------------------");
-    }else{
-      System.out.println(" Compilation SUCCESS! ");
-      System.out.println("-----------------------------------------");
-    }
-
   }
 }
