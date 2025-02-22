bc4d59c70a9bf7852a539a675cc52ba3f727196b
Vitor <vitor.laguardia@hotmail.com>
2025-02-22 12:32:58 -0300

    criado factory de semantic exception

--- ExceptionFactory.java
+++ ExceptionFactory.java
@@ -6,4 +6,8 @@ public class ExceptionFactory {
   public static LexicalException createLexicalException(String message, int line) {
     return new LexicalException(message, line);
   }
+
+  public static SemanticException createSemanticException(String message, int line) {
+    return new SemanticException(message, line);
+  }
 }
